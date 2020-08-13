e76e09e186297bccecede6825df8c573cd9beb31
Dragana Carapic <dragana.carapic98@uns.ac.rs>
2020-08-13 18:01:05 +0200

    #add new: guest_page.html and edit: apartments.html

---
+++ PocetniREST/WebContent/guest_navbar.js
@@ -0,0 +1,32 @@
+document.write(`
+		
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <h4 class="navbar-brand" id="title" style="margin-right: 50px;">DSTOURS.COM</h4>
+
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Rezervišite smještaj</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Moje rezervacije</a>
+    </li>
+	
+    
+  </ul>
+  <ul class="navbar-nav ml-auto nav-flex-icons" style="margin-right: 50px;">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+          aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-user"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right dropdown-default"
+          aria-labelledby="navbarDropdownMenuLink-333">
+          <a class="dropdown-item" href="#">Izmijeni profil</a>
+          <a class="dropdown-item" href="#">Odjavi se</a>
+        </div>
+      </li>
+    </ul>
+</nav>	
+		
+`);
